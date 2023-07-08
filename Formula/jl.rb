69cd66005f68d461f12ced54f76b029c6e5e7559
Koen Bollen <meneer@koenbollen.nl>
2023-07-08 13:11:52 +0200

    Bump jl to 1.6.0

--- Formula/jl.rb
+++ Formula/jl.rb
@@ -1,8 +1,8 @@
 class Jl < Formula
   desc "JSON Logs, a development tool for working with structured JSON logging."
   homepage "https://github.com/koenbollen/jl"
-  url "https://github.com/koenbollen/jl/releases/download/v1.5.0/jl_darwin_amd64"
-  sha256 "cb570eafa65e5bddb224f485f7376e08e19e943b9c032ef9c827f53f2e1af46f"
+  url "https://github.com/koenbollen/jl/releases/download/v1.6.0/jl_darwin_amd64"
+  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
 
 
   def install
