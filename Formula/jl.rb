685384bc4bfc1af6cfc87b1e50e65735e7e778a7
Koen Bollen <meneer@koenbollen.nl>
2023-07-10 12:30:25 +0200

    Fix sha for jl 1.6.0
    The jl tag was misspelled and I shasum'd a 404 response from Github.

--- Formula/jl.rb
+++ Formula/jl.rb
@@ -2,7 +2,7 @@ class Jl < Formula
   desc "JSON Logs, a development tool for working with structured JSON logging."
   homepage "https://github.com/koenbollen/jl"
   url "https://github.com/koenbollen/jl/releases/download/v1.6.0/jl_darwin_amd64"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  sha256 "1f9561d89c13cfcdb1814d3b8bca827b541c6b6bfa1ef7bf88af1c175ecf77cf"
 
 
   def install
